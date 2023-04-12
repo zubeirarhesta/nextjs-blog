a3163271b2762aaff965175847a49ace4c7dc28f
zubeirarhesta <zubeirarhesta@gmail.com>
2023-04-12 19:41:48 +0700

    make another change

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-export const name = "Zubeir Muhammad Arhesta";
+export const name = "Zubeir";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/profile2.JPG"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
